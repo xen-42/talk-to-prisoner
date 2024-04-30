f7fa62df81fff74cc6c674d4cd3f470fd031279e
Nick <nicholasconnors@hotmail.ca>
2024-04-30 00:18:51 -0400

    why dont this work

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# talk-to-prisoner
-Most important mod ever made
+# You can now play as Gabbro
+
+Features:
+- Spawn on GD with your ship
+- Spawn hatchling on TH
+- Altered the first loop
+
+Plans:
+- Change traveler dialogue to refer to you as Gabbro
+  - A lot of dialogue is very Hatchling specific as well, might have to just change everything
+- Add dialogue with the Hatchling
+  - Needs specific dialogue as well for the first loop, and for the first time you speak to them after dying
+- Change the interaction with Self (or at least just the model)
---
+++ TalkToPrisoner/TalkToPrisoner.cs
@@ -0,0 +1,63 @@
+﻿using HarmonyLib;
+using NewHorizons;
+using OWML.Common;
+using OWML.ModHelper;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace TalkToPrisoner
+{
+    public class TalkToPrisoner : ModBehaviour
+    {
+        public static TalkToPrisoner Instance { get; private set; }
+
+        public INewHorizons NewHorizons { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public static void Log(string message) => Instance.ModHelper.Console.WriteLine(message);
+        public static void FireOnNextUpdate(Action action) => Instance.ModHelper.Events.Unity.FireOnNextUpdate(action);
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(TalkToPrisoner)} is loaded!", MessageType.Success);
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+            // Get the New Horizons API and load configs
+            NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                FireOnNextUpdate(() =>
+                {
+                    var prisoner = GameObject.FindObjectOfType<PrisonerBrain>().gameObject.GetComponentInChildren<CharacterDialogueTree>();
+                    var dialogueAssetName = prisoner._xmlCharacterDialogueAsset.name;
+                    var xml = File.ReadAllText(ModHelper.Manifest.ModFolderPath + "/planets/dialogue.xml");
+                    var dialogueInfo = "{ \"pathToExistingDialogue\": \"" + GetPath(prisoner.transform).Remove(0, "DreamWorld_Body/".Length) + "\", \"xmlFile\": \"planets/dialogue.xml\" }";
+                    NewHorizons.CreateDialogueFromXML(dialogueAssetName, xml, dialogueInfo, Locator.GetAstroObject(AstroObject.Name.DreamWorld).gameObject);
+                });
+
+            };
+        }
+
+        private string GetPath(Transform t)
+        {
+            if (t.parent == null)
+            {
+                return t.name;
+            }
+            else
+            {
+                return GetPath(t.parent) + "/" + t.name;
+            }
+        }
+    }
+}
